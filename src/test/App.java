602f88636bd1c5c9189551daf3d5b069e76f281c
victor zitacuaro <victorzitacuaro18@gmail.com>
2026-01-13 09:20:15 -0800

    save2

--- src/test/App.java
+++ src/test/App.java
@@ -5,16 +5,23 @@ import Package1.myClass; // imports the methods and information from package 1 s
 public class App {
     public static void main(String[] args) {
 
-        int a = 0;
-        // declare and initialize a list of names
-        String[] listOfNames = { "Alice", "Bob", "Charlie", "Dennis" };
+        String[][] listOfNames = { { "Alice", "Bob", "Charlie", "Dennis" }, { "E", "F", "G", "H" } }; // 2-D array. this
+                                                                                                      // is a 2x4 array.
+                                                                                                      // 2 sets of array
+                                                                                                      // with 4 values
+                                                                                                      // in each array.
+        for (int i = 0; i < listOfNames.length; i++) {
+            for (int j = 0; j < listOfNames[i].length; j++) {
 
-        for (int i = 0; i < listOfNames.length; i++)
-            System.out.println(listOfNames[i]);
+                System.out.println("Name :" + listOfNames[i][j] + " ");
+            }
 
-        // System.out.println("Name: " + listOfNames[0] + "," + listOfNames[1] + "," +
-        // listOfNames[2]);
+        }
+        Car c = new Car(2026); // calls the the "Car" class and gives it the variable called c. the year for
+                               // car
+                               // c is 2026.
+        int carYearOfManufacturer = 2023;
+        System.out.println("Year of manufacture:2023" + carYearOfManufacturer);
     }
 
-    myClass c = new myClass();
 }
---
+++ src/test/Car.java
@@ -0,0 +1,13 @@
+package test;
+
+public class Car {
+    private int year;
+
+    public Car(int year) {
+        this.year = year;
+    }
+
+    public int getYear() {
+        return year;
+    }
+}
